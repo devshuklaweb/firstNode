1c8bf937d2707538ed19f509162524094051e34c
Devendra Shukla <devshukla0123@gmail.com>
2024-06-03 12:48:41 +0530

    code for getting data using external files

---
+++ data.js
@@ -0,0 +1,8 @@
+const data = [
+    {name:'Devendra',email:'dev@gmail.com',age:22},
+    {name:'Devendra22',email:'dev22@gmail.com',age:23},
+    {name:'Devendra23',email:'dev23@gmail.com',age:24},
+    {name:'Devendra24',email:'dev24@gmail.com',age:25}
+];
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //creating a basic api with static data
 const http = require("http");
+const data = require("./data");
 http.createServer((req,res)=>{
     //create header and mention return type
     res.writeHead(200,{'Content-Type':'application\json'});
-    res.write(JSON.stringify({name:'Devendra',email:'dev@gmail.com',age:22}));
+    res.write(JSON.stringify(data));
     res.end();
     console.log("start server on post: http://localhost:4000");
 
